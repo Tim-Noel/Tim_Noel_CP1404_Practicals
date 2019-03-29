d37d3258b8c18e0891a9bb5c17654b9f13210e67
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-03-30 09:39:38 +1000

    Created main() function

--- Prac_04/quick_picks.py
+++ Prac_04/quick_picks.py
@@ -9,3 +9,11 @@ NUMBER_PER_LINE = 6
 MIN = 1
 MAX = 45
 
+def main():
+    number_of_quick_picks = int(input("How many quick picks?: "))
+    while number_of_quik_picks <0:
+        print("Please insert a valid response")
+        number_of_quick_picks = int(input("How many quick picks?: "))
+
+
+main()
