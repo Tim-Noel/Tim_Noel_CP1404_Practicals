60d4defba86f46cef0cbf4c7ba754620728ad0bb
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-03-30 09:05:05 +1000

    Added print functions to exercise: Basic List Operations.

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -9,6 +9,11 @@ for i in range(5):
     number = int(input("Please insert number: "))
     numbers.append(number)
 
+print("The first number is ", numbers[0])
+print("The last number is ", numbers[-1])
+print("The smallest number is ", min(numbers))
+print("The largest number is ", max(numbers))
+print("The average of the numbers is ", sum(numbers) / len(numbers))
 
 
 # 2) Woefully Inadequate Security Checker
