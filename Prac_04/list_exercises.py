4bded3f350fea1d13ac6fd50342032a4d0d98ca5
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-03-30 09:08:28 +1000

    Completed Woefully Inadequate Security Checker exercise.

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -15,5 +15,11 @@ print("The smallest number is ", min(numbers))
 print("The largest number is ", max(numbers))
 print("The average of the numbers is ", sum(numbers) / len(numbers))
 
-
 # 2) Woefully Inadequate Security Checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("please enter an username: ")
+if username in usernames:
+    print("welcome ", username)
+else:
+    print("access denied, you are not welcome", username)
