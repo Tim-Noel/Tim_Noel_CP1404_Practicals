46b9ae7e6c35b64505f147ec75648ce80ba01121
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-03-30 09:00:57 +1000

    Created for loop and append for exercise: Basic List Operations.

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -1,5 +1,14 @@
 """
 CP1404 Prac
-Basic List Operations
+Basic List Operations and Woefully Inadequate Security Checker
 """
 
+# 1) Basic List Operations
+numbers = []
+for i in range(5):
+    number = int(input("Please insert number: "))
+    numbers.append(number)
+
+
+
+# 2) Woefully Inadequate Security Checker
