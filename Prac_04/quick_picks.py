3e78af08a083ada1e279734b7fb32dd447a272f2
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-03-30 10:06:44 +1000

    fixed typo and completed quick_picks

--- Prac_04/quick_picks.py
+++ Prac_04/quick_picks.py
@@ -9,11 +9,23 @@ NUMBER_PER_LINE = 6
 MIN = 1
 MAX = 45
 
+
 def main():
     number_of_quick_picks = int(input("How many quick picks?: "))
-    while number_of_quik_picks <0:
+    while number_of_quick_picks < 0:
         print("Please insert a valid response")
         number_of_quick_picks = int(input("How many quick picks?: "))
+
+    for i in range(number_of_quick_picks):
+        quick_pick = 1
+        for x in range(NUMBER_PER_LINE):
+            number = random.randint(MIN, MAX)
+            while number in quick_pick:
+                number - random.randint(MIN, MAX)
+            quick_pick.apped(number)
+        quick_pick.sort()
+
+        print(" ".join("{:2}".format(number) for number in quick_pick))
 
 
 main()
