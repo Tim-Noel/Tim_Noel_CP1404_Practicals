41ef5715d3c70f29a9c665f9f3e31a1ca012adf7
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-04-08 13:30:51 +1000

    words are now added to counted_words, counted and printed.

--- Prac_05/word_occurences.py
+++ Prac_05/word_occurences.py
@@ -11,3 +11,9 @@ for word in words:
     frequency = counted_word.get(word, 0)
     counted_word[word] = frequency + 1
 
+words = list(counted_word.keys())
+words.sort()
+max_length = max((len(word) for word in words))
+
+for word in words:
+    print("{:{}} : {}".format(word, max_length, counted_word[word]))
