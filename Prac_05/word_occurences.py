0f7a34c93b329b8a5d88383c5ba4b3087cd20bc1
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-04-08 13:04:35 +1000

    created variables and for loop

--- Prac_05/word_occurences.py
+++ Prac_05/word_occurences.py
@@ -2,3 +2,12 @@
 CP1404/CP5632 Prac
 Counts the amount of times a word appears in a string
 """
+
+counted_word = {}
+text = input("Text: ")
+words = text.split()
+
+for word in words:
+    frequency = counted_word.get(word, 0)
+    counted_word[word] = frequency + 1
+
