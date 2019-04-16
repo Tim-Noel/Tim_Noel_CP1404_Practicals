8c1ee9e458bf15ad2c49edfcbd8fd7fc06aa7d44
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-04-17 09:24:31 +1000

    Completed main function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,8 +5,18 @@ Guitar collection records
 
 
 def main():
+    """Guitar program, using Guitar class."""
     guitars = []
+
     print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add, "added.")
+        name = input("Name: ")
 
 
 main()
