2c0b4df5db9ea373c4b66930cb5c62f76108e272
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-04-17 09:15:19 +1000

    Fixed Type, languages.py functioning correctly

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -12,7 +12,7 @@ def main():
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
     languages = [ruby, python, visual_basic]
-    print("The languages are:")
+    print("The dynamically typed languages are:")
     for language in languages:
         if language.is_dynamic():
             print(language.name)
