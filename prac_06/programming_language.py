8135780dd1cb25607d2be5aa3dcddbe7a0dbf55e
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-04-16 19:34:53 +1000

    Created constructor and is_dynamic function

---
+++ prac_06/programming_language.py
@@ -0,0 +1,15 @@
+"""
+CP1404 Prac_06
+
+"""
+
+
+class ProgrammingLanguage:
+    def __init__(self):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
