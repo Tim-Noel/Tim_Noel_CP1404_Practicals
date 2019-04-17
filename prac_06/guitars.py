b036e304f8b98a19da3f13e3b6d7e374be83aa61
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-04-17 10:46:01 +1000

    Added testing guitars, Gibson and JTV

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -2,10 +2,14 @@
 CP1404/CP5632 Prac
 Guitar collection records
 """
+from prac_06.guitar import Guitar
 
 
 def main():
     guitars = []
+
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     print("My guitars!")
     name = input("Name: ")
