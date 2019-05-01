35044015feb8df5b72fa4689d7aaeade41dfcbf9
TIm_Noel <tim.noel@my.jcu.edu.au>
2019-05-01 14:54:50 +1000

    Created initialize, build and button_creater functions within the Button_Maker class

--- prac_07/button_maker.py
+++ prac_07/button_maker.py
@@ -9,6 +9,19 @@ from kivy.uix.button import Button
 from kivy.properties import StringProperty
 
 
-class Button_Maker:
+class Button_Maker(App):
     def __init__(self):
         self.word_list = ("Herbert", "William", "Susan")
+
+    def build(self):
+        self.title = "Button Maker"
+        self.root = Builder.load_file("button_maker.kv")
+        self.button_creater()
+        return self.root
+
+    def button_creator(self):
+        for word in self.word_list:
+            new_button = Button(text=word, id=word)
+            new_button.bind()
+
+Button_Maker().run()
