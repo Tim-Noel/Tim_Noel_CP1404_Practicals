859aaacd55237a88918970038ecaa9fffcddfe09
Tim-Noel <tim.noel@my.jcu.edu.au>
2019-05-02 10:43:18 +1000

    Added the MilesToKilometres class along with the build and convert_to_miles function.

--- prac_07/convert_miles_to_km.py
+++ prac_07/convert_miles_to_km.py
@@ -3,3 +3,21 @@ CP1404
 converts miles to kilometres
 """
 
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+MILES_PER_KILOMETRES = 1.609
+
+class MilesToKilometers(App):
+    def build(self):
+        Window.size = (400, 200)
+        self.title = "Miles to kilometres Converter"
+        self.root = Builder.load_file('convert_miles_to_km.kv')
+        return self.root
+
+    def convert_miles_to_kilometers(self):
+        value = self.validate_input()
+        result = value * MILES_PER_KILOMETRES
+        self.root.ids.output_label.text = str(result)
+
